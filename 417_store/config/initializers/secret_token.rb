3bc7b9cdf0ba4815a24030d400e843d5bc977ed4
misogi <dev@misogi.info>
2013-07-11 14:43:52 +0900

    learning ZURB Foundation

---
+++ 417_store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '3bd65c5b8b2a3763b893244bd9ae028c580f466f095e94aebf1ea24ee87face285d0266c64587b5df60d190a4ab1320820ddd40b74828081ce9fbebbfba310dc'
