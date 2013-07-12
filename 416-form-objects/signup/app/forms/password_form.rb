5aab4dbf0b26035f05c1003f75a5bbff4d77fed4
misogi <dev@misogi.info>
2013-07-12 14:52:43 +0900

    implove to 16:35 in movie

--- 416-form-objects/signup/app/forms/password_form.rb
+++ 416-form-objects/signup/app/forms/password_form.rb
@@ -8,22 +8,31 @@ class PasswordForm
   end
 
   attr_accessor :changing_password, :original_password, :new_password
-  validate :verify_original_password, if: :changing_password
-  validates_presence_of :original_password, :new_password, if: :changing_password
-  validates_confirmation_of :new_password, if: :changing_password
-  validates_length_of :new_password, minimum: 6, if: :changing_password
+  validate :verify_original_password
+  validates_presence_of :original_password, :new_password
+  validates_confirmation_of :new_password
+  validates_length_of :new_password, minimum: 6
 
   def initialize(user)
     @user = user
+  end
+
+  def submit(params)
+    self.original_password = params[:original_password]
+    self.new_password = params[:new_password]
+    self.new_password_confirmation = params[:new_password_confirmation]
+    if valid?
+      @user.password = new_password
+      @user.save!
+      true
+    else
+      false
+    end
   end
 
   def verify_original_password
     unless @user.authenticate(original_password)
       errors.add :original_password, "is not correct"
     end
-  end
-
-  def change_password
-    @user.password = new_password
   end
 end
---
+++ 416-form-objects/signup/app/forms/signup_form.rb
@@ -0,0 +1,53 @@
+class SignupForm
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  def persisted?
+    false
+  end
+
+  def self.model_name
+    ActiveModel::Name.new(self, nil, "User")
+  end
+
+  validates_presence_of :username
+  validate :verify_unique_username
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
+  validates_length_of :password, minimum: 6
+
+  delegate :username, :email, :password, :passord_confirmation, to: :user
+  delegate :twitter_name, :github_name, :bio, to: :profile
+
+  def user
+    @user ||= User.new
+  end
+
+  def profile
+    @profile ||= user.build_profile
+  end
+
+  def submit(params)
+    user.attributes = params.slice()
+  end
+
+  def subscribed
+    user.subscribed_at
+  end
+
+  def subscribed=(checkbox)
+    user.subscribed_at = Time.zone.now if checkbox == "1"
+  end
+
+  def generate_token
+    begin
+      user.token = SecureRandom.hex
+    end while User.exists?(token: token)
+  end
+
+  def verify_unique_username
+    if User.exists? username: username
+      errors.add :username, "has already been taken"
+    end
+  end
+end
