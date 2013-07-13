c2f4ee057679c61945ab352cf59e3f7bffa9b567
misogi <dev@misogi.info>
2013-07-13 20:02:27 +0900

    inprove to 9:20 in movie

--- 115-model-caching-revised/blog/app/models/article.rb
+++ 115-model-caching-revised/blog/app/models/article.rb
@@ -3,4 +3,26 @@ class Article < ActiveRecord::Base
   has_many :comments
   attr_accessible :name, :published_at, :content
   scope :published, -> { where("published_at < ?", Time.zone.now) }
+
+  after_commit :flush_cache
+
+  def self.cached_find(id)
+    Rails.cache.fetch([name, id], expires_in: 5.minutes){find(id)}
+  end
+
+  def flush_cache
+    Rails.cache.delete([self.class.name, id])
+  end
+
+  def cached_comments_count
+    Rails.cache.fetch([self, "comments_count"]) {comments.size}
+  end
+
+  def cached_comments
+    Rails.cache.fetch([self, "comments"]) {comments.to_a}
+  end
+
+  def cached_author
+    Author.cached_find(author_id)
+  end
 end
