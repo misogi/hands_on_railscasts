7839f43d5749b67e9f8434600531b1d344700f2e
misogi <dev@misogi.info>
2013-07-17 11:32:18 +0900

    refactor store with dinamic template of error page

--- 053-handling-exceptions-revised/store/config/application.rb
+++ 053-handling-exceptions-revised/store/config/application.rb
@@ -58,5 +58,8 @@ module Store
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_dispatch.rescue_responses["ProductsController::Forbidden"] = :forbidden
+    config.exceptions_app = self.routes
   end
 end
--- 053-handling-exceptions-revised/store/config/routes.rb
+++ 053-handling-exceptions-revised/store/config/routes.rb
@@ -1,4 +1,5 @@
 Store::Application.routes.draw do
   resources :products
   root to: 'products#index'
+  match ':status', to: 'errors#show', constraints: {status: /\d{3}/}# via: :all
 end
