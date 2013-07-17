3a492591c88ddf49b78cfbf97abab77d65198780
misogi <dev@misogi.info>
2013-07-17 11:32:45 +0900

    commit fogetted file

--- 053-handling-exceptions-revised/store/app/controllers/application_controller.rb
+++ 053-handling-exceptions-revised/store/app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # rescue_from "Exception", with: :forbidden
+  # #rescue_from "ProductsController::Forbidden", with: :forbidden
+
+  # private
 
   # def forbidden(exception)
+  #   render text: exception.message
+  # end
 end
