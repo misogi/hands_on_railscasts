0beb8b2458a5e57fb6d75c421cac9eb3d3bdb6f0
misogi <dev@misogi.info>
2013-08-10 16:42:53 +0900

    finish video #409

---
+++ 409-active-model-serializers/blog/app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @article}
+    end
+  end
+
+
+end
+
